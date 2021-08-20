5ed04fb0603909242b9b0a77778115d0ff4fa5dd
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2021-08-20 21:16:19 +0200

    Algorithm refactor

--- schedules/makeSchedule.js
+++ schedules/makeSchedule.js
@@ -1,4 +1,4 @@
-import { schedule2 } from "../courses.js";
+import { courses, schedule, schedule2 } from "../courses.js";
 import { fitness } from "./fitness.js";
 import { compareMaps, sortSchedules } from "./schedulesUtil.js";
 
@@ -9,42 +9,38 @@ const getRandonInt = (max) => {
 export const getSchedule = (rounds, combinations, options) => {
   let schedules = [];
   let existNum = 0;
-
-  for (let index = 0; index < rounds; index++) {
-    const courses = [];
-    combinations.forEach((course) => {
-      courses.push(course[getRandonInt(course.length)]);
-    });
-
-    if (fitness(courses, options).fit >= 0) {
-      const newSchedule = fitness(courses, options);
-      let exist = false;
-      // schedules.forEach((schedule) => {
-      //   if (compareMaps(schedule.schedule, newSchedule.schedule)) {
-      //     // index--;
-      //     exist = true;
-      //     // break
-      //   }
-      // });
-      exist = schedules.find(
-        (schedule) =>
-          JSON.stringify(schedule.schedule) ===
-          JSON.stringify(newSchedule.schedule)
-      );
-      if (!exist) {
-        schedules.push(newSchedule);
-        console.log(true);
-      } else {
-        console.log(existNum);
-        existNum++;
-        if (existNum > 2000) {
-          break;
+  const allPossibleCombinations = (combinations) => {
+    if (combinations.length === 0) {
+      return [];
+    } else if (combinations.length === 1) {
+      return combinations[0];
+    } else {
+      var result = [];
+      var allCasesOfRest = allPossibleCombinations(combinations.slice(1)); // recur with the rest of array
+      allCasesOfRest.forEach((comb) => {
+        for (var i = 0; i < combinations[0].length; i++) {
+          let combination = [];
+          !Array.isArray(comb)
+            ? combination.push(comb)
+            : combination.push(...comb);
+          combination.push(combinations[0][i]);
+          result.push(combination);
         }
-      }
+      });
+      return result;
     }
-  }
+  };
 
-  console.log(schedules.length);
+  const scheduleCombinations = allPossibleCombinations(combinations);
+
+  console.log(scheduleCombinations);
+
+  scheduleCombinations.forEach((combination) => {
+    if (fitness(combination, options).fit >= 0) {
+      const newSchedule = fitness(combination, options);
+      schedules.push(newSchedule);
+    }
+  });
 
   schedules.forEach((schedule1, index1) => {
     schedules.slice(index1 + 1).forEach((schedule2, index2) => {
--- testSchedules.js
+++ testSchedules.js
@@ -3,18 +3,72 @@ import schedules from "./test.js";
 
 let exist = false;
 
-for (let i = 0; i < schedules.length; i++) {
-  schedules.slice(i + 1).forEach((schedule, index) => {
-    // console.log(index + i + 1);
-    if (JSON.stringify(schedule) === JSON.stringify(schedules[i])) {
-      exist = true;
-    }
-  });
-  if (exist) {
-    break;
-  }
-  // console.log(i);
-}
+// for (let i = 0; i < schedules.length; i++) {
+//   schedules.slice(i + 1).forEach((schedule, index) => {
+//     // console.log(index + i + 1);
+//     if (JSON.stringify(schedule) === JSON.stringify(schedules[i])) {
+//       exist = true;
+//     }
+//   });
+//   if (exist) {
+//     break;
+//   }
+//   // console.log(i);
+// }
 
-console.log(schedules.length);
-console.log(exist);
+// console.log(schedules.length);
+// console.log(exist);
+
+const allPossibleCombinations = (combinations) => {
+  if (combinations.length === 0) {
+    return [];
+  } else if (combinations.length === 1) {
+    return combinations[0];
+  } else {
+    var result = [];
+    var allCasesOfRest = allPossibleCombinations(combinations.slice(1)); // recur with the rest of array
+    // console.log(allCasesOfRest);
+    allCasesOfRest.forEach((comb) => {
+      for (var i = 0; i < combinations[0].length; i++) {
+        let combination = [];
+        console.log(comb, Array.isArray(comb) ? "array" : "obj");
+        !Array.isArray(comb)
+          ? combination.push(comb)
+          : combination.push(...comb);
+        combination.push(combinations[0][i]);
+        result.push(combination);
+      }
+    });
+    return result;
+  }
+};
+const combinations = [
+  [
+    { a: 1, b: 2 },
+    { c: 1, d: 2 },
+  ],
+  [
+    { a: 3, b: 4 },
+    { c: 3, d: 4 },
+    { e: 3, f: 4 },
+  ],
+  [
+    { a: 5, b: 6 },
+    { c: 5, d: 6 },
+    { o: 5, m: 6 },
+  ],
+  [
+    { a: 5, b: 6 },
+    { c: 5, d: 6 },
+    { o: 5, m: 6 },
+  ],
+  [
+    { a: 5, b: 6 },
+    { c: 5, d: 6 },
+    { o: 5, m: 6 },
+  ],
+];
+
+const result = allPossibleCombinations(combinations);
+
+console.log(JSON.stringify(result));
