2f9a6e53e306100b80984c743f8bb8c484763523
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2021-08-20 13:19:24 +0200

    Courses added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Course from "../components/Course.js";
 import Schedule from "../components/Schedule.js";
-
 import { Button } from "react-bootstrap";
 
 import axios from "axios";
