eba11222808101542e369010c577df45f9fefb19
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2021-09-23 02:04:17 +0200

    Redux in production disable

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,5 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunk from "redux-thunk";
 import reduceReducers from "reduce-reducers";
 
